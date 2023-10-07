11f11213f69b6644218c0d9058091101432d5cf3
Tahir <133495698+tqh1r@users.noreply.github.com>
2023-10-07 19:36:31 +0200

    made code more readable

--- register-login system.py
+++ register-login system.py
@@ -5,16 +5,20 @@ while True:
     print("Dobrodosli, kreirajte svoj nalog.")
     user = input("Unesite svoje ime: ")
     password = input("Unesite svoju sifru: ")
+
     if "123" in password:
         print("Nesigurna sifra, probajte ponovo!")
         break        
+
     PIN = random.randint(100, 999)
     print("Vas PIN je:", PIN)
     PIN = str(PIN)
+
     confirmation = input("Da li ste sigurni? (Da/Ne): ")
     if confirmation.lower() == "da":
         print("Kreiranje naloga je uspelo.")
         break
+
     if confirmation.lower() == "ne":
         print("Kreiranje naloga nije uspjelo.")
         break
@@ -31,16 +35,18 @@ while True:
     if userlogin != user:
         print("Netacno ime, probajte ponovo!")
         break
+
     passwordlogin = input("Unesite vasu sifru: ")
     if passwordlogin != password:
         print("Netacna sifra, probajte ponovo!")
         break
+
     pinlogin = input("Unesite vas PIN kod: ")
     if pinlogin != PIN:
         print("Netacan PIN kod, probajte ponovo.")
         break
+    
     if pinlogin == PIN:
         print("Uspesno smo se prijavili na nalog.")
         break
-    if pinlogin == PIN:
-        print("Uspesno smo se prijavili na nalog.")
+    
